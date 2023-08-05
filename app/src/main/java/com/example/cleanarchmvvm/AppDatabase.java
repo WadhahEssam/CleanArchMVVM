637561b5f942032e08f4c641ed690e6cfe4c7335
Wadah Esam <wadah-wleed@hotmail.com>
2023-08-05 18:35:55 +0300

    addinga a database creation callback to populate the database

--- app/src/main/java/com/example/cleanarchmvvm/AppDatabase.java
+++ app/src/main/java/com/example/cleanarchmvvm/AppDatabase.java
@@ -1,10 +1,14 @@
 package com.example.cleanarchmvvm;
 
+import android.content.AsyncQueryHandler;
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 // @Database defines the whole database of your app and what tables it has
 // the version is helpful whenever you make changes to the database, you can
@@ -13,9 +17,7 @@ import androidx.room.RoomDatabase;
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance; // because we are going to make this class singleton
 
-
     public abstract NoteDao noteDao();
-
 
     // this is just a general way of creating singletons
     // synchronized means that only one thread can create
@@ -28,10 +30,32 @@ public abstract class AppDatabase extends RoomDatabase {
                                 AppDatabase.class,
                                 "note_database") // this is telling room the name of the database
                         .fallbackToDestructiveMigration() // this is telling room the mechanism of migrating, now if there is a new version, it will just destroy the previous one
+                        .addCallback(roomCallback) // when the room database is first created, it will run the async task that we just defined
                         .build();
         }
         return instance;
     }
 
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+        }
+    };
 
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(AppDatabase db) {
+            noteDao = db.noteDao(); // this is possible because it is created after our database
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("title 1", "Description 1", 1));
+            noteDao.insert(new Note("title 2", "Description 2", 3));
+            noteDao.insert(new Note("title 3", "Description 3", 1));
+            return null;
+        }
+    }
 }
